5745a0996a62ebeb5df93c72a08474cd91d74a27
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2021-07-25 06:28:33 +0200

    Initial Commit

---
+++ Kontry_SwiftUI/Kontry_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kontry_SwiftUIApp.swift
+//  Kontry_SwiftUI
+//
+//  Created by  Ahmed Shendy on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Kontry_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
